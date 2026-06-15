acbe19dc92d85ec14dea24a9e6b17cbc7da54b81
Edelytics <iitmjanakpuridelhi@gmail.com>
2026-06-15 22:12:17 +0530

    history page

--- aboutus_new/history.php
+++ aboutus_new/history.php
@@ -17,7 +17,8 @@ echo '</script>';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>IITM</title>
+    <title>History & Legacy of Excellence | IITM Janakpuri </title>
+<meta name="description" content="Discover the rich history of IITM Janakpuri, known for academic excellence, innovation, research-driven learning, and career-focused education.">
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
