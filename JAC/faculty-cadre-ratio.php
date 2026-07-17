eabbca6e68f01c489efa24821ea9fca5d5023b35
Rohit Kumar <rjangra696@gmail.com>
2026-07-17 11:17:42 +0530

    feat: add faculty cadre ratio assessment page

---
+++ JAC/faculty-cadre-ratio.php
@@ -0,0 +1,38 @@
+<?php
+$jac_page_title = 'Faculty Cadre Ratio';
+$jac_active     = 'faculty-cadre-ratio.php';
+include 'jac-header.php';
+?>
+<nav class="jac-crumb"><a href="https://www.iitmjanakpuri.com/index.php">Home</a> &rsaquo; <a href="mandatory-disclosure.php">Joint Assessment Committee</a> &rsaquo; FCR</nav>
+<h1 class="jac-h1">Parameter IIB(ii): Faculty Cadre Ratio</h1>
+<p class="jac-lead">Appointment of faculty in the cadre of Professor, Associate Professor and Assistant Professor as per statutory guidelines.</p>
+
+<div class="doc-scroll">
+    <table class="doc-table">
+        <thead>
+            <tr>
+                <th style="width: 20%;">Programme</th>
+                <th style="width: 12%; text-align: center;">Required</th>
+                <th style="width: 12%; text-align: center;">Professors</th>
+                <th style="width: 12%; text-align: center;">Associate Prof.</th>
+                <th style="width: 12%; text-align: center;">Assistant Prof.</th>
+                <th style="width: 16%; text-align: center;">Ratio achieved</th>
+                <th style="width: 16%; text-align: center;">Verification Link</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr><td>MBA</td><td style="text-align: center;">6</td><td style="text-align: center;">1</td><td style="text-align: center;">1</td><td style="text-align: center;">4</td><td style="text-align: center;">1:1:4</td><td style="text-align: center;"><a href="https://www.iitmjanakpuri.com/mandatory/pdf/2B_Cadre_MBA.pdf" target="_blank"><i class="fas fa-file-pdf"></i> View Details</a></td></tr>
+            <tr><td>MCA</td><td style="text-align: center;">6</td><td style="text-align: center;">1</td><td style="text-align: center;">1</td><td style="text-align: center;">4</td><td style="text-align: center;">1:1:4</td><td style="text-align: center;"><a href="https://www.iitmjanakpuri.com/mandatory/pdf/2B_Cadre_MCA.pdf" target="_blank"><i class="fas fa-file-pdf"></i> View Details</a></td></tr>
+            <tr><td>BBA</td><td style="text-align: center;">54</td><td style="text-align: center;">3</td><td style="text-align: center;">9</td><td style="text-align: center;">42</td><td style="text-align: center;">1:3:14</td><td style="text-align: center;"><a href="https://www.iitmjanakpuri.com/mandatory/pdf/2B_Cadre_BBA.pdf" target="_blank"><i class="fas fa-file-pdf"></i> View Details</a></td></tr>
+            <tr><td>BCA</td><td style="text-align: center;">36</td><td style="text-align: center;">2</td><td style="text-align: center;">6</td><td style="text-align: center;">28</td><td style="text-align: center;">1:3:14</td><td style="text-align: center;"><a href="https://www.iitmjanakpuri.com/mandatory/pdf/2B_Cadre_BCA.pdf" target="_blank"><i class="fas fa-file-pdf"></i> View Details</a></td></tr>
+            <tr><td>B.Com (Hons.)</td><td style="text-align: center;">18</td><td style="text-align: center;">1</td><td style="text-align: center;">3</td><td style="text-align: center;">14</td><td style="text-align: center;">1:3:14</td><td style="text-align: center;"><a href="https://www.iitmjanakpuri.com/mandatory/pdf/2B_Cadre_BCom.pdf" target="_blank"><i class="fas fa-file-pdf"></i> View Details</a></td></tr>
+            <tr><td>BA (JMC)</td><td style="text-align: center;">18</td><td style="text-align: center;">1</td><td style="text-align: center;">3</td><td style="text-align: center;">14</td><td style="text-align: center;">1:3:14</td><td style="text-align: center;"><a href="https://www.iitmjanakpuri.com/mandatory/pdf/2B_Cadre_BAJMC.pdf" target="_blank"><i class="fas fa-file-pdf"></i> View Details</a></td></tr>
+        </tbody>
+    </table>
+</div>
+
+<div class="doc-note">
+    <strong>Note on Cadre Ratio Factor (CRF):</strong><br>
+    Marks are awarded based on CRF (f) = (K - SF actually in position) / SF actually in position, where SF is Senior Faculty (Professors + Associate Professors). Ideal case f &le; 2.
+</div>
+<?php include 'jac-footer.php'; ?>
