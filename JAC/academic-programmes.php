642458c20fda3547484f2ebeea06d30896c65dd1
Rohit Kumar <rjangra696@gmail.com>
2026-07-17 11:17:41 +0530

    feat: add academic programme details and sanctioned intake table

---
+++ JAC/academic-programmes.php
@@ -0,0 +1,54 @@
+<?php
+$jac_page_title = 'Details of Academic Programmes';
+$jac_active     = 'academic-programmes.php';
+include 'jac-header.php';
+?>
+        <nav class="jac-crumb"><a href="https://www.iitmjanakpuri.com/index.php">Home</a> &rsaquo; <a href="mandatory-disclosure.php">Joint Assessment Committee</a> &rsaquo; Details of Academic Programmes</nav>
+        <h1 class="jac-h1">B. Details of Academic Programmes</h1>
+        <p class="jac-lead">Programmes offered by the Institute of Information Technology &amp; Management, their year of commencement, regulatory bodies and sanctioned intake for the assessment year.</p>
+
+        <h2 class="jac-h2">(a) Academic Programmes &mdash; Sanctioned Intake &amp; Admissions</h2>
+        <div class="doc-scroll">
+            <table class="doc-table">
+                <thead>
+                    <tr>
+                        <th rowspan="2" style="width: 6%; text-align: center; vertical-align: middle;">S. No.</th>
+                        <th rowspan="2" style="width: 32%; vertical-align: middle;">Programme</th>
+                        <th rowspan="2" style="width: 12%; text-align: center; vertical-align: middle;">Start Year</th>
+                        <th colspan="2" style="width: 16%; text-align: center;">Academic Session 2024–25</th>
+                        <th colspan="2" style="width: 16%; text-align: center;">Academic Session 2025–26</th>
+                        <th rowspan="2" style="width: 18%; text-align: center; vertical-align: middle;">Intake Applied A.Y. 2026–27</th>
+                    </tr>
+                    <tr>
+                        <th style="text-align: center;">Intake</th>
+                        <th style="text-align: center;">Admitted</th>
+                        <th style="text-align: center;">Intake</th>
+                        <th style="text-align: center;">Admitted</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr><td style="text-align: center;">1.</td><td>MBA</td><td style="text-align: center;">2012</td><td style="text-align: center;">60</td><td style="text-align: center;">60</td><td style="text-align: center;">60</td><td style="text-align: center;">59</td><td style="text-align: center;">60</td></tr>
+                    <tr><td style="text-align: center;">2.</td><td>MCA</td><td style="text-align: center;">2023</td><td style="text-align: center;">60</td><td style="text-align: center;">60</td><td style="text-align: center;">60</td><td style="text-align: center;">60</td><td style="text-align: center;">60</td></tr>
+                    <tr><td style="text-align: center;">3.</td><td>BCA (First Shift)</td><td style="text-align: center;">1999</td><td style="text-align: center;">120</td><td style="text-align: center;">120</td><td style="text-align: center;">120</td><td style="text-align: center;">120</td><td style="text-align: center;">120</td></tr>
+                    <tr><td style="text-align: center;">4.</td><td>BCA (Second Shift)</td><td style="text-align: center;">2008</td><td style="text-align: center;">120</td><td style="text-align: center;">120</td><td style="text-align: center;">120</td><td style="text-align: center;">120</td><td style="text-align: center;">120</td></tr>
+                    <tr><td style="text-align: center;">5.</td><td>BBA (First Shift)</td><td style="text-align: center;">2003</td><td style="text-align: center;">180</td><td style="text-align: center;">180</td><td style="text-align: center;">180</td><td style="text-align: center;">180</td><td style="text-align: center;">180</td></tr>
+                    <tr><td style="text-align: center;">6.</td><td>BBA (Second Shift)</td><td style="text-align: center;">2007</td><td style="text-align: center;">180</td><td style="text-align: center;">180</td><td style="text-align: center;">180</td><td style="text-align: center;">180</td><td style="text-align: center;">180</td></tr>
+                    <tr><td style="text-align: center;">7.</td><td>B.Com (Hons.) (First Shift)</td><td style="text-align: center;">2017</td><td style="text-align: center;">60</td><td style="text-align: center;">60</td><td style="text-align: center;">60</td><td style="text-align: center;">58</td><td style="text-align: center;">60</td></tr>
+                    <tr><td style="text-align: center;">8.</td><td>B.Com (Hons.) (Second Shift)</td><td style="text-align: center;">2019</td><td style="text-align: center;">60</td><td style="text-align: center;">60</td><td style="text-align: center;">60</td><td style="text-align: center;">59</td><td style="text-align: center;">60</td></tr>
+                    <tr><td style="text-align: center;">9.</td><td>BA (JMC)</td><td style="text-align: center;">2023</td><td style="text-align: center;">120</td><td style="text-align: center;">120</td><td style="text-align: center;">120</td><td style="text-align: center;">118</td><td style="text-align: center;">120</td></tr>
+                </tbody>
+            </table>
+        </div>
+
+        <h2 class="jac-h2">(b) Programme-wise List of Faculty</h2>
+        <p>Programme-wise list of faculty for the assessment year, uploaded under mandatory disclosures.</p>
+        <div class="doc-dl">
+            <a href="https://www.iitmjanakpuri.com/mandatory/pdf/b)%20Programmewise%20list%20of%20Faculty.pdf" target="_blank" rel="noopener"><i class="fas fa-file-pdf"></i> View Faculty List</a>
+        </div>
+
+        <h2 class="jac-h2">(c) Programme-wise Class-wise Time Table</h2>
+        <p>Programme-wise, class-wise time table for the assessment year, uploaded under mandatory disclosures.</p>
+        <div class="doc-dl">
+            <a href="https://www.iitmjanakpuri.com/mandatory/pdf/c_TimeTable_EvenOdd.pdf" target="_blank" rel="noopener"><i class="fas fa-file-pdf"></i> View Time Tables</a>
+        </div>
+<?php include 'jac-footer.php'; ?>
